4001bb15d008df71f8914ad00f9c96b06ae67f8c
Rozario D'souza <77534438+Rozario29@users.noreply.github.com>
2022-04-06 21:40:58 +0530

    Update and rename main.py to .py

--- .py
+++ .py
@@ -1,3 +1,5 @@
+## Code for concatenating with strings
+
 print("Welcome to the Band Name Generator.")
 street = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
